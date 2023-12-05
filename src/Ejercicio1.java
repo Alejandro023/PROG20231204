098a8fc0958a216580301ff9a763bbf924d60404
Alejandro023 <148380656+Alejandro023@users.noreply.github.com>
2023-12-05 19:53:23 +0100

    fin examen

---
+++ src/Ejercicio1.java
@@ -0,0 +1,13 @@
+
+public class Ejercicio1 {
+	
+	// si recibe la cadena "hola"
+	// retorna la cadena "h**a"
+	public static String transformar(String s) {
+		s.charAt(0) + s.length + 2;
+		
+	}
+
+	
+	
+}
